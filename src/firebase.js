6b0eeffe59e83bc95685e424ed301c5ec0818f77
H. Utku Ekrem <110349452+uekrem@users.noreply.github.com>
2024-03-19 00:13:14 +0300

    mail-Integration

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import toast from "react-hot-toast";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB0sXHnrlY4eXnW3X9dOUhkBHKjOiTo4H8",
-  authDomain: "myecommerce-1dd77.firebaseapp.com",
-  projectId: "myecommerce-1dd77",
-  storageBucket: "myecommerce-1dd77.appspot.com",
-  messagingSenderId: "111688240978",
-  appId: "1:111688240978:web:ed77d7dc3142a80393348b"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 const app = initializeApp(firebaseConfig);
